d88fb57db48135b9d0b6ae368319010a214a78b8
andrew-yazbeck <andrew.yazbeck@stu.ocsb.ca>
2025-09-16 13:27:44 -0400

    shows the area and perimeter of a rectangle

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Andrew
+ * Created on: Sep 2025
+ * This program shows the area and perimeter of a rectangle
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A rectangle with dimensions 5 cm & 3 cm.')
+basic.pause(100)
+basic.clearScreen()
+
+// perimeter
+basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString() + ' cm')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+// area
+basic.showString('The area would be:' + (5 * 3).toString() + ' cm^2')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
